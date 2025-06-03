eb47719d9590c992c5a6c17059fd6d7ca4cce2dc
lordace-coder <lordyacey@gmail.com>
2025-06-03 19:33:08 +0200

    fix import err

--- pinetwork/settings.py
+++ pinetwork/settings.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
